ebd0f14b38909a2831b1cceabe81f6c015bcd5f2
Karolina <karolina@rantfors.se>
2018-04-06 17:06:04 +0200

    6_april

---
+++ client/src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('./api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+console.log('Environment', process.env.NODE_ENV);
